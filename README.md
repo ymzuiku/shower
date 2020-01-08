ddbe68759f395f74d0a95ae9d77b67718e049018
梁柱 <ymblender@gmail.com>
2020-01-08 21:02:01 +0800

    1

---
+++ README.md
@@ -0,0 +1,12 @@
+## Shower
+
+```dart
+// don't render
+Shower(none: true, child: Text('hello'));
+
+// hidden render
+Shower(hidden: true, child: Text('hello'));
+
+// offscreen render
+Shower(offscreen: true, child: Text('hello'));
+```
---
+++ lib/shower.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/widgets.dart';
+
+///  none > hidden > offscreen
+class Shower extends StatelessWidget {
+  final Widget child;
+  final bool none;
+  final bool offscreen;
+  final bool hidden;
+
+  Shower({
+    @required this.child,
+    this.none,
+    this.offscreen,
+    this.hidden,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (none == true) {
+      return SizedBox(width: 0, height: 0);
+    }
+    if (hidden == true) {
+      return new IgnorePointer(
+        ignoring: true,
+        child: new Opacity(
+          opacity: 0.0,
+          child: child,
+        ),
+      );
+    }
+    if (offscreen == true) {
+      return new Offstage(
+        offstage: true,
+        child: child,
+      );
+    }
+
+    return child;
+  }
+}
