8f80a8f02824a860122db16758db2e4e957da491
梁柱 <ymblender@gmail.com>
2020-01-29 02:01:33 +0800

    2

--- lib/shower.dart
+++ lib/shower.dart
@@ -38,3 +38,41 @@ class Shower extends StatelessWidget {
     return child;
   }
 }
+
+class SliderShower extends StatelessWidget {
+  final Widget child;
+  final bool none;
+  final bool offscreen;
+  final bool hidden;
+
+  SliderShower({
+    @required this.child,
+    this.none,
+    this.offscreen,
+    this.hidden,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (none == true) {
+      return SizedBox(width: 0, height: 0);
+    }
+    if (hidden == true) {
+      return IgnorePointer(
+        ignoring: true,
+        child: Opacity(
+          opacity: 0.0,
+          child: child,
+        ),
+      );
+    }
+    if (offscreen == true) {
+      return Offstage(
+        offstage: true,
+        child: child,
+      );
+    }
+
+    return child;
+  }
+}
